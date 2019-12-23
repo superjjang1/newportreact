90f71135c3ef0a527876951f224e780dc90e6512
Sean Yim <superjjang89@gmail.com>
2019-12-23 15:43:13 -0500

    bootstrap and navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -8,20 +8,28 @@ class NavBar extends Component {
     render() { 
         return ( 
             <div>
-                <ul>
-                    <li>
-                        hello
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Sean Yim</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">About me <span class="sr-only">(current)</span></a>
                     </li>
-                    <li>
-                        this
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Projects</a>
                     </li>
-                    <li>
-                        is 
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Skills</a>
                     </li>
-                    <li>
-                        insane
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Links</a>
                     </li>
-                </ul>
+                    </ul>
+                </div>
+                </nav>
             </div>
          );
     }
