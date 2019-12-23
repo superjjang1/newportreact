b674772da88e8b19338dafbe710d7cdd16ed605e
Sean Yim <superjjang89@gmail.com>
2019-12-23 15:58:07 -0500

    tying in a hero/aboutme

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/navbar/NavBar';
+import AboutMe from './components/AboutMe/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      
+      <AboutMe/>
     </div>
   );
 }
---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class AboutMe extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className="container-fluid">
+                <h1>hello?</h1>
+            </div>
+        );
+    }
+}
+ 
+export default AboutMe;
