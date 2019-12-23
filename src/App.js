d97bd2c5b13de91fbf1d3c2df34eb1109535aa60
Sean Yim <superjjang89@gmail.com>
2019-12-23 15:32:55 -0500

    navbar skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <header className="App-header">
         sanity check
       </header>
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <ul>
+                    <li>
+                        hello
+                    </li>
+                    <li>
+                        this
+                    </li>
+                    <li>
+                        is 
+                    </li>
+                    <li>
+                        insane
+                    </li>
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default NavBar;
