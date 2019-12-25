bbf9b4ff5ecc1591d1397a1f16896f55704f9dba
Sean Yim <superjjang89@gmail.com>
2019-12-24 23:12:43 -0500

    about me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,8 +10,18 @@ class AboutMe extends Component {
         return ( 
             <div className="container-fluid">
                 <div className="home" id="Home">
-                    <h1>Hello?</h1>
-                    
+                    <div className="row">
+                        <h2 className="heading">
+                            About Me
+                        </h2>
+                        <div>
+                            My name is Sean Yim, a former English Teacher, and former assistant manager at a retail <p/>
+                            paint store. Now, I'm pursuing a career as a full-time software developer, hoping to create new <p/>
+                            and innovative applications. From a career in sales and a background in literature, to pursuing <p/>
+                            my true passion, software development.<p/>
+                            I've always enjoyed learning new things, and software development is a landscape that's constantly changing. 
+                        </div>
+                    </div>
                 </div>
             </div>
         );
