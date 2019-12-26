30b448fe592241f5660739637417515abf4e7a18
Sean Yim <superjjang89@gmail.com>
2019-12-26 12:21:04 -0500

    styling a bit with bootstrap

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,19 +8,24 @@ class AboutMe extends Component {
     }
     render() { 
         return ( 
-            <div className="container-fluid">
+            <div className="container">
                 <div className="home" id="Home">
                     <div className="row">
-                        <h2 className="heading">
-                            About Me
-                        </h2>
-                        <div>
-                            My name is Sean Yim, a former English Teacher, and former assistant manager at a retail <p/>
-                            paint store. Now, I'm pursuing a career as a full-time software developer, hoping to create new <p/>
-                            and innovative applications. From a career in sales and a background in literature, to pursuing <p/>
-                            my true passion, software development.<p/>
-                            I've always enjoyed learning new things, and software development is a landscape that's constantly changing. 
+                        <div className="col">
+                            <h2 className="heading shadow-sm p-3 mb-5 bg-white rounded">
+                                About Me
+                            </h2>
                         </div>
+                        <div className="w-100"></div>
+                        
+                            <div className="col text-left">
+                                My name is Sean Yim, a former English Teacher, and former assistant manager at a retail 
+                                paint store. Now, I'm pursuing a career as a full-time software developer, hoping to create new 
+                                and innovative applications. From a career in sales and a background in literature, to pursuing 
+                                my true passion, software development.
+                                I've always enjoyed learning new things, and software development is a landscape that's constantly changing. 
+                            </div>
+                        <div className="w-100"></div>
                     </div>
                 </div>
             </div>
