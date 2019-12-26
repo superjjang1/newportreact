7a52e8cdc6b5bec5e784317203a6f24b7cc27d6f
Sean Yim <superjjang89@gmail.com>
2019-12-26 13:15:42 -0500

    links

--- src/components/Links/Links.js
+++ src/components/Links/Links.js
@@ -10,8 +10,26 @@ class Links extends Component {
         return ( 
             <div className="container-fluid">
                 <div className="links" id="Links">
-                    <h1>Hello? links</h1>
-                    
+                    <div className="row">
+                        <div className="col">
+                            <h2 className="heading">Find me here</h2>
+                        </div>
+                        <div className="w-100"></div>
+                            <div className="col-sm">
+                            <a href="https://www.linkedin.com/in/sean-yim-80b509118/" target="_blank">
+                                <i className="fab fa-linkedin fa-2x"> Linkedin</i></a>
+                            </div>
+                            <div className="col-sm">
+                            <a href="https://www.github.com/superjjang1" target="_blank">
+                                <i class="fab fa-github fa-2x"> Github</i></a>
+                            </div>
+                            <div className="col-sm">
+                                <a href="https://drive.google.com/file/d/1AQ1vP46IL_B9A4BbhDlkuPD1k3m8cBEg/view?usp=sharing" target="_blank">
+                                    <i className="fas fa-paperclip fa-2x"> Resume</i>
+                                </a>
+                            </div>
+                        
+                    </div>
                 </div>
             </div>
          );
