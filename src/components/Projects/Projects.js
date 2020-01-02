dec94fd62f9078f795e2f4b91030c0dd25401a9e
Sean Yim <superjjang89@gmail.com>
2020-01-02 16:40:19 -0500

    carousel for projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,7 +15,24 @@ class Projects extends Component {
                             <h2 className="heading shadow-sm p-3 mb-5 bg-white rounded">Projects</h2>
                         </div>
                         <div className="w-100"></div>
-                        <div className="col-m"></div>
+                        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img src="https://i.imgur.com/nvLNmz7.png" className="d-block w-100 h-100" alt="sweets"/>
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="https://i.imgur.com/GFGtoR3.png" className="d-block w-100 h-100" alt="gametime"/>
+                                </div>
+                            </div>
+                            <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Previous</span>
+                            </a>
+                            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Next</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
